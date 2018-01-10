9316b5e82d6251ef4216bd48a45d83b66df449ab
Febby315 <564822672@qq.com>
2018-01-10 16:53:59 +0800

    优化页面下载脚本的

--- spider/2_spider_pick_list/phantomjs/PageDownloader.js
+++ spider/2_spider_pick_list/phantomjs/PageDownloader.js
@@ -1,76 +1,52 @@
 "use strict";
-var page = require('webpage').create();
-var system = require('system');
+phantom.outputEncoding = "utf-8";
+//加载第三方库及配置
+var webpage = require("webpage");
+var system = require("system");
+var config = require("../utils/config");
 
-var config = require('../utils/config');
+//获取传入参数并解析
+var params = JSON.parse(system.args[1]||"{}");
 
-phantom.outputEncoding="utf-8";
-page.settings.loadImages = false;	//禁止加载图片
+//创建并配置抓取窗口
+var page = webpage.create();
+page.settings.loadImages = false; //禁止加载图片
+page.customHeaders = { Referer: params.url };
 
-var params={};
-function init(){
-	 params=JSON.parse(system.args[1]);
-}
-init();
-page.customHeaders = {
-	"Referer":params.url,
-};
-		var json = {
-				status:99,//失败
-				_id:params.id,
-				version:params.version
-		};
-setTimeout(function(){  //配置超时退出
-	send(json);
-	phantom.exit(999);
-},3*60000);
+//配置超时退出(放弃抓取并标记失败)
+setTimeout(function() {
+    json.status=99;
+    send(json);
+    phantom.exit(999);
+}, 60*1000);
 
-function send(json){
-	var settings = {
-		  operation: "POST",
-		  encoding: "utf8",
-		  headers: {
-			"Content-Type": "application/json"
-		  },
-		  data: JSON.stringify(json)
-		};
-		  console.log(json);
-	page.open(config.db_server_url+config.db_list_source_edit,settings,function(status) {
-		if(status.toUpperCase()==="SUCCESS"&&page.plainText.toUpperCase()==="SUCCESS"){
-			//setTimeout(phantom.exit(0),1000);
-		}else{
-			//setTimeout(phantom.exit(1),1000);
-		}
-	});
+//初始化数据
+var json = { status: 99, _id: params.id, version: params.version };
+
+//数据传回服务器
+function send(json) {
+    var options = {
+        operation: "POST", encoding: "utf8",
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify(json)
+    };
+    //打开新窗口调用接口将数据传回服务器后退出当前进程
+    webpage.create().open(config.db_server_url + config.db_list_source_edit,options,function(status) {
+        setTimeout(phantom.exit(status.toUpperCase() === "SUCCESS"?0:1),1000);
+    });
 }
 
-////////////////////////////////////////////////////////////////////////////////
-console.log('status',params.url);
-//
+//发起页面并等待渲染后抓取页面
 page.open(params.url, function(status) {
-	console.log('status',status);
-  if (status !== 'success') {
-	  console.log(json);
-	phantom.exit();
-  }else{
-		try{
-			setTimeout(function(){
-				var ua = page.evaluate(function() {
-					  return document.getElementsByTagName('html')[0].innerHTML;
-					});
-							json['list_content'] = ua;//爬取内容
-							json['status'] = 1;//状态为已爬取
-							send(json);
-			},15000);
-		}catch(e){
-				send(json);
-			console.log('phantomjs 出错了',e);
-		}
-		setTimeout(function(){
-						phantom.exit(0);
-		},35000);
-  } 
-  
+    if (status !== "success") {
+        json.status=99;
+        send(json);
+    }else{
+        //访问成功后等待15秒抓取页面
+        setTimeout(function() {
+            json["list_content"] = page.evaluate(function() { return document.getElementsByTagName('html')[0].outerHTML; });
+            json.status = 1;
+            send(json);
+        }, 15000);
+    }
 });
-
-//
--- spider/4_spider_pick_detail/phantomjs/PageDownloader.js
+++ spider/4_spider_pick_detail/phantomjs/PageDownloader.js
@@ -1,69 +1,52 @@
 "use strict";
-phantom.outputEncoding="utf-8";
+phantom.outputEncoding = "utf-8";
+//加载第三方库及配置
+var webpage = require("webpage");
+var system = require("system");
+var config = require("../utils/config");
 
-var Browser = require('webpage');
-var system = require('system');
-var config = require('../utils/config');
+//获取传入参数并解析
+var params = JSON.parse(system.args[1]||"{}");
 
-var params={};
-function initParams(){  //初始化参数
-	params=JSON.parse(system.args[1]||"{}");
-}
-initParams();
+//创建并配置抓取窗口
+var page = webpage.create();
+page.settings.loadImages = false; //禁止加载图片
+page.customHeaders = { Referer: params.url };
 
-var json = {};
-json['_id'] = params.id;//状态为已爬取
-json['version'] = params.version;//状态为已爬取
-json['dealStatus'] = 99;//爬取状态 默认爬取失败99
+//配置超时退出(放弃抓取并标记失败)
+setTimeout(function() {
+    json.status=99;
+    send(json);
+    phantom.exit(999);
+}, 60*1000);
 
-setTimeout(function(){  //配置超时退出
-	send(json);
-	phantom.exit(999);
-},3*60000);
+//初始化数据
+var json = { status: 99, _id: params.id, version: params.version };
 
-function send(json){
-	var settings = {
-		operation: "POST",
-		encoding: "utf8",
-		headers: { "Content-Type": "application/json" },
-		data: JSON.stringify(json)
-	};
-	var sendPage = Browser.create();
-	sendPage.open(config.db_server_url+config.db_list_detail_source_edit,settings,function(status) {
-		if(status.toUpperCase()==="SUCCESS"){   //&&page.plainText.toUpperCase()==="SUCCESS"
-			phantom.exit(0);//正常退出
-		}else{
-			phantom.exit(1);//接口访问失败
-		}
-	});
+//数据传回服务器
+function send(json) {
+    var options = {
+        operation: "POST", encoding: "utf8",
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify(json)
+    };
+    //打开新窗口调用接口将数据传回服务器后退出当前进程
+    webpage.create().open(config.db_server_url + config.db_list_source_edit,options,function(status) {
+        setTimeout(phantom.exit(status.toUpperCase() === "SUCCESS"?0:1),1000);
+    });
 }
 
-var page=Browser.create();
-page.settings.loadImages = false;	//禁止加载图片
-
+//发起页面并等待渲染后抓取页面
 page.open(params.url, function(status) {
-	  if (status !== 'success') {
-	phantom.exit();
-  }else{
-		
-		setTimeout(function(){
-		var ua = page.evaluate(function() {
-			  return document.getElementsByTagName('html')[0].innerHTML;
-			});
-
-					json['content_sour'] = ua;//爬取内容
-					json['dealStatus'] = 1;//状态为已爬取
-					json['detail_url'] = page.url;//有跳转的改成跳转后的地址
-					send(json);
-					console.log("url "+params.url);	
-
-
-},15000);
-
-setTimeout(function(){
-				phantom.exit(0)
-},25000);
-  } 
-  
-
+    if (status !== "success") {
+        json.status=99;
+        send(json);
+    }else{
+        //访问成功后等待15秒抓取页面
+        setTimeout(function() {
+            json["list_content"] = page.evaluate(function() { return document.getElementsByTagName('html')[0].outerHTML; });
+            json.status = 1;
+            send(json);
+        }, 15000);
+    }
 });
